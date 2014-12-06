10a62aa15bec234bd9666c0ee0f3c49ff3947f2f
kothead <gdmmtf@gmail.com>
2014-12-06 23:51:13 +0500

    assets packing

---
+++ core/src/com/kothead/ld31/data/Configuration.java
@@ -0,0 +1,11 @@
+package com.kothead.ld31.data;
+
+/**
+ * Created by st on 12/6/14.
+ */
+public class Configuration {
+
+    public static final int WORLD_WIDTH = 640;
+    public static final int WORLD_HEIGHT = 320;
+    public static final int SCALE_FACTOR = 1;
+}
--- desktop/src/com/kothead/ld31/desktop/DesktopLauncher.java
+++ desktop/src/com/kothead/ld31/desktop/DesktopLauncher.java
@@ -2,11 +2,44 @@ package com.kothead.ld31.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.kothead.ld31.LD31;
+import com.kothead.ld31.data.Configuration;
+
+import java.io.File;
+import java.io.FileFilter;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
+		packAssets();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Configuration.WORLD_WIDTH;
+		config.height = Configuration.WORLD_HEIGHT;
 		new LwjglApplication(new LD31(), config);
+	}
+
+	private static void packAssets() {
+		File dir = new File("../../images");
+
+		FileFilter filter = new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		};
+
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxWidth = 1024;
+		settings.maxHeight = 1024;
+		settings.edgePadding = true;
+		settings.duplicatePadding = true;
+		settings.paddingX = 4;
+		settings.paddingY = 4;
+
+		for (File childDir: dir.listFiles(filter)) {
+			TexturePacker.process(settings, childDir.getPath(), "data", childDir.getName());
+		}
+
 	}
 }
