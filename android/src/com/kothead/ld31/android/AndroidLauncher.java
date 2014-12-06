e67ae8d89560ca076072708c1cc3f0bdcc3bf21a
kothead <gdmmtf@gmail.com>
2014-12-06 12:35:27 +0500

    initial commit

---
+++ android/src/com/kothead/ld31/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kothead.ld31.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kothead.ld31.LD31;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD31(), config);
+	}
+}
---
+++ desktop/src/com/kothead/ld31/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kothead.ld31.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kothead.ld31.LD31;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD31(), config);
+	}
+}
